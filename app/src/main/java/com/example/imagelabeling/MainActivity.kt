3a44508d172d31900c0d308c335ddfc8ee021b11
sat0004 <satyam13arora1999@gmail.com>
2019-12-28 18:38:04 +0530

    initialized fritz sdk

--- app/src/main/java/com/example/imagelabeling/MainActivity.kt
+++ app/src/main/java/com/example/imagelabeling/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.imagelabeling
 
+import ai.fritz.core.Fritz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val API_KEY = "dcf6227d8ecf4968b4e1a1b5fc1c483b"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //initialize fritz sdk
+        Fritz.configure(this, API_KEY)
     }
 }
