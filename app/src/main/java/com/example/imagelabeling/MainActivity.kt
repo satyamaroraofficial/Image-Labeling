369d71df45ef40802f11b16794f7cf6a13f8045b
sat0004 <satyam13arora1999@gmail.com>
2019-12-29 16:00:15 +0530

    Added api keys

--- app/src/main/java/com/example/imagelabeling/MainActivity.kt
+++ app/src/main/java/com/example/imagelabeling/MainActivity.kt
@@ -1,18 +1,49 @@
 package com.example.imagelabeling
 
 import ai.fritz.core.Fritz
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val API_KEY = "dcf6227d8ecf4968b4e1a1b5fc1c483b"
+    val apikey = BuildConfig.ApiKey
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //initialize fritz sdk
-        Fritz.configure(this, API_KEY)
+        Fritz.configure(this, apikey)
+
+//        view_finder.post {
+//            startCamera()
+//        }
+    }
+
+    //Creating and displaying the camera preview
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder()
+            .apply {
+                setTargetResolution(Size(1920, 1080))
+            }.build()
+
+        //generate a preview
+        val preview = Preview(previewConfig)
+
+        //Add a listener to update preview automatically
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = view_finder.parent as ViewGroup
+
+            //remove the old preview
+            parent.removeView(view_finder)
+            //add the new preview
+            parent.addView(view_finder, 0)
+            view_finder.surfaceTexture = it.surfaceTexture
+        }
     }
 }
